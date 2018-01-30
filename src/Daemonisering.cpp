a58adc6a02e2382fac13488343c9ac5dd83c2a68
andersreiche <andersreiche@gmail.com>
2018-01-30 12:19:33 +0100

    working on syslog

--- src/Daemonisering.cpp
+++ src/Daemonisering.cpp
@@ -12,52 +12,62 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-int main(int argc, char* argv[])
-{
-FILE *fp= NULL;
-pid_t process_id = 0;
-pid_t sid = 0;
+#include <signal.h>
+
+void sig_handler(int signo);
+
+int main(int argc, char* argv[]) {
+	FILE *fp = NULL;
+	pid_t process_id = 0;
+	pid_t sid = 0;
 // Create child process
-process_id = fork();
+	process_id = fork();
 // Indication of fork() failure
-if (process_id < 0)
-{
-printf("fork failed!\n");
+	if (process_id < 0) {
+		printf("fork failed!\n");
 // Return failure in exit status
-exit(1);
-}
+		exit(1);
+	}
 // PARENT PROCESS. Need to kill it.
-if (process_id > 0)
-{
-printf("process_id of child process %d \n", process_id);
+	if (process_id > 0) {
+		printf("process_id of child process %d \n", process_id);
 // return success in exit status
-exit(0);
-}
+		exit(0);
+	}
 //unmask the file mode
-umask(0);
+	umask(0);
 //set new session
-sid = setsid();
-if(sid < 0)
-{
+	sid = setsid();
+	if (sid < 0) {
 // Return failure
-exit(1);
-}
+		exit(1);
+	}
 // Change the current working directory to root.
-chdir("/");
+	chdir("/");
 // Close stdin. stdout and stderr
-close(STDIN_FILENO);
-close(STDOUT_FILENO);
-close(STDERR_FILENO);
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
 // Open a log file in write mode.
-fp = fopen ("Log.txt", "w+");
-while (1)
-{
+	fp = fopen("Log.txt", "w+");
+	while (1) {
 //Dont block context switches, let the process sleep for some time
-sleep(1);
-fprintf(fp, "Logging info...\n");
-fflush(fp);
+		sleep(1);
+		fprintf(fp, "Logging info...\n");
+		fflush(fp);
+
+		if (signal(SIGTERM, sig_handler) == SIG_ERR) {
+			fprintf(fp, "received SIGTERM, exiting process\n");
+			fflush(fp);
+			printf("\ncan't catch SIGINT\n");
+		}
 // Implement and call some function that does core work for this daemon.
+	}
+	fclose(fp);
+	return (0);
 }
-fclose(fp);
-return (0);
+
+void sig_handler(int signo) {
+	if (signo == SIGTERM)
+		exit(0);
 }
